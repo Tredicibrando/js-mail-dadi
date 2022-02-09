ed7754496276cba069db30141419bbd295a065b8
Tredicibrando <nicolosigiov@gmail.com>
2022-02-09 17:27:00 +0100

    mail

--- js/app.js
+++ js/app.js
@@ -1,1 +1,37 @@
-console.log('ciao')
+
+let userMail = document.getElementById('input_email');
+console.log(userMail)
+
+const mailArray = [
+    'nicolosigiov@gmail.com', 
+    'simone@boolean.com', 
+    'giovanni@libero.it',
+    'davide@yahoo.de'
+    ]
+
+    console.log(mailArray)
+
+    let trovataMail = false;
+    
+    for (let i = 0; i < mailArray.length; i++) {
+        
+        if (userMail === mailArray[i] ){
+            trovataMail = true; 
+        } 
+    }
+    const stampa = document.querySelector('.success')
+    if (trovataMail){
+        
+        stampa.classList.add('active')
+    
+    } 
+    
+    
+
+
+
+
+
+    
+        
+    
