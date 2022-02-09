6e296a45e8de4765bca3ee5ea1b072b83d0df877
Tredicibrando <nicolosigiov@gmail.com>
2022-02-09 18:40:28 +0100

    parte mail

--- js/app.js
+++ js/app.js
@@ -1,30 +1,44 @@
 
-let userMail = document.getElementById('input_email');
+let userMail = document.getElementById('email')
+const buttonVai = document.getElementById('email_vai')
+const mailUtente= 
+
+buttonVai.addEventListener('click', function(){
+
+    const mailUtente = userMail.value
+
+    console.log(mailUtente)
+
+    const mailArray = [
+        'nicolosigiov@gmail.com', 
+        'simone@boolean.com', 
+        'giovanni@libero.it',
+        'davide@yahoo.de'
+        ]
+    
+        console.log(mailArray)
+    
+        let trovataMail = false;
+        
+        for (let i = 0; i < mailArray.length; i++) {
+            
+            
+            if (mailUtente === mailArray[i] ){
+                trovataMail = true; 
+            } 
+        }
+        const stampa = document.querySelector('.success')
+
+        if (trovataMail){
+            
+            alert('Login effettuato con successo')
+            
+        } 
+})
+
 console.log(userMail)
 
-const mailArray = [
-    'nicolosigiov@gmail.com', 
-    'simone@boolean.com', 
-    'giovanni@libero.it',
-    'davide@yahoo.de'
-    ]
 
-    console.log(mailArray)
-
-    let trovataMail = false;
-    
-    for (let i = 0; i < mailArray.length; i++) {
-        
-        if (userMail === mailArray[i] ){
-            trovataMail = true; 
-        } 
-    }
-    const stampa = document.querySelector('.success')
-    if (trovataMail){
-        
-        stampa.classList.add('active')
-    
-    } 
     
     
 
